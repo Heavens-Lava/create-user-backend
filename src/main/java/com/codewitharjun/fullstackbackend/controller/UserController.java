3069c1245d8ba4ba71b2dd9c8930f27fa130ee8a
Jeffrey Macy <macy.jeffreyj@gmail.com>
2022-10-26 20:56:10 -0700

    change 2

--- src/main/java/com/codewitharjun/fullstackbackend/controller/UserController.java
+++ src/main/java/com/codewitharjun/fullstackbackend/controller/UserController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+//@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://springboot-create-user-front.herokuapp.com")
 public class UserController {
 
     @Autowired
